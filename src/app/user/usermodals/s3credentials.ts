92e5282e6c2522e8c18dcc890c9046d541e387e7
Milan Rawat <rawatbleachrock@gmail.com>
2020-05-17 14:22:57 +0530

    firstcommit

--- src/app/user/usermodals/s3credentials.ts
+++ src/app/user/usermodals/s3credentials.ts
@@ -1,6 +1,6 @@
 export const BUCKET={
-    accessKeyId: 'AKIAI26ZIUX2EYWBO6FQ',
-    secretAccessKey: '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',
-    region: 'us-east-2',
-    bucketname:'uploadtravbucket'
+    accessKeyId: '',
+    secretAccessKey: '',
+    region: '',
+    bucketname:''
 }
